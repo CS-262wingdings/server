c663af257b604b69d83a71db4d1577c74e079656
Ian Park <duckonomy@gmail.com>
2018-11-29 20:01:06 -0500

    sql

--- questions.sql
+++ questions.sql
@@ -5,14 +5,9 @@ CREATE TABLE Question (
 	--Contents is the string of the question itself.
 	contents varchar(200),
 	--Timestamp lets us list the questions by new.
-        author varchar(20),
 	time timestamp,
 	--Downloads tracks the popularity of a question based on how many people downloaded it.
-	downloads integer,
-	--ratings stores how many people have rated the question.
-	ratings integer,
-	--currentRating is the decimal rating of the question.
-	currentRating real);
+	downloads integer);
 
 -- Make sure people can access table
 GRANT SELECT ON Question TO PUBLIC;
@@ -20,10 +15,7 @@ GRANT SELECT ON Question TO PUBLIC;
 INSERT INTO Question VALUES (
         DEFAULT,
         'On an average day, how many pigeons do you think you could reasonably carry?',
-        'XxPigeonMaster99Xx',
-        298,
-        49,
-        9.89,
-        NOW());
+        NOW(),
+        298);
 
 SELECT * FROM Question;
